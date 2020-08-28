6612289af0644fa180411f0ec62ff5e0681d86be
DanielnetoDotCom <danielneto.com@gmail.com>
2020-08-28 01:25:38 -0300

    y

--- static/on_publish.php
+++ static/on_publish.php
@@ -1,2 +1,25 @@
 <?php
 error_log(json_encode($_POST));
+
+$url = '{webSiteRootURL}plugin/Live/on_publish.php';
+
+//url-ify the data for the POST
+$fields_string = http_build_query($_POST);
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, 1);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+$output = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpcode);
+header("HTTP/1.1 $httpcode");
